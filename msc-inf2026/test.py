1fa860a40f32ad680cc6dd54b6d678668dade779
Minas Dasygenis <mdasyg@ieee.org>
2026-04-24 19:02:50 +0300

    g

--- msc-inf2026/test.py
+++ msc-inf2026/test.py
@@ -1,5 +1,93 @@
-import pygame 
-print("Hello, World!This is a test file for the MSC-INF2026 course.")
+#import pygame 
+#print("Hello, World!This is a test file for the MSC-INF2026 course.")
 
+ 
+#x=1
+#y=1.0
+#z="This is a string"
+#w=True
+
+#print("The value of x is:", x)
+#print(f"The value of y is: {y}")          
+
+
+#
+
+#age=input("Please enter your age: ")
+#years10= int(age) +10 
+#print(years10)
+# + - /  * ** // % 
+#print(float(5//2))
+#print(5/2)
+#str(4)
+my="Hello Dr. Dasygenis"
+#print(my[0:10:2])
+#print(my.count("l"))
+
+# == != > < >= <=
+#x=1
+#y=2
+#z=3
+#print( x == y or y>x)
+
+#x=input("Name: ")
+#if x=="Alice":
+#    print("Hello, boss!")
+#elif x=="Bob":
+#   print("Hello, Bob!")    
+#else:
+#    print("Hello, stranger!")   
+#print("This will always be printed.")
+
+
+z=[]
+x=[1,True,"Hello",3.14 ]
+
+#print(len(x))
+#print(x[2])
+#x[3]=2.718
+#x.append(3.14159)
+#print(x)
+#x.pop(1)
+#print(x)
+
+x=(1,True,"Hello",3.14)
+#print(x[2])
+
+#[2]="World"
+
+#for inum in x:
+#    print("VALUE found: ", inum)
+#
+#for inum in range(0,10,2):
+#    print("VALUE found: ", inum)
+
+#i=0
+#while i<10:  
+#    print("VALUE found: ", i)
+#    i+=3
+
+#x={"name":"Alice", "age":30, "is_student":False}
+#print(x["name"])
+#x["name"]="Bob"
+#print(x["name"])
+
+
+def greet(name):
+    print(f"Hello, {name}!")
+
+
+#greet("Alice")
+#greet("Bob")
+#greet("Charlie")
+
+def add(a, b):
+    x="internal x is there"
+    print(x)
+    return a + b    
+
+print(x)
+print(add(3, 5))    
+print(x)
 
 
